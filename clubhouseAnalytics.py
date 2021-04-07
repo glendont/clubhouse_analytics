999522d9b76137961b74624da255538b652890d1
Mackenzie Kosut <mkosut@gmail.com>
2021-04-07 13:25:14 -0400

    actual commit -- cleaned up logic, more readable, added logging, re-added time extraction

--- clubhouseAnalytics.py
+++ clubhouseAnalytics.py
@@ -1,12 +1,17 @@
 import boto3
 import time
-import psutil
+import re
+import logging
+import time
+import datetime
+
 from botocore.config import Config
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s')
 
 s3 = boto3.resource('s3')
 textract = boto3.client('textract')
 my_bucket = s3.Bucket('clubhouse-stats')
-
 session = boto3.Session()
 write_client = session.client('timestream-write', config=Config(
     read_timeout=20, max_pool_connections=5000, retries={'max_attempts': 10}))
@@ -15,64 +20,67 @@ query_client = session.client('timestream-query')
 # Document
 s3BucketName = "clubhouse-stats"
 
+ROOM = "AWS Startups"
+# Format DD/MM/YYYY
+DATE_OF_EVENT = "2-4-2021"
 DATABASE_NAME = "Clubhouse"
 TABLE_NAME = "participants"
-USER = 1
+USER = ""
 
-def write(ActiveUser):
+def write(USER, ROOM_TIME):
 
-    dimensions = [
-        {'Name': 'room', 'Value': "AWS Startups"},
-        {'Name': 'Participant', 'Value': "Test"}
+  logging.debug("Preparing record for %s", USER)
+  logging.info("The time is %s", ROOM_TIME)
+  common_attributes = {
+      'Dimensions': [
+          {'Name': 'User', 'Value': USER},
+          {'Name': 'Room', 'Value': ROOM}
     ]
+  }
 
-    records = []
-    current_time = int(time.time() * 1000)
+  record = [{
+    'MeasureName': 'Active',
+    'MeasureValue': '1',
+    'Time': str(ROOM_TIME),
+    'TimeUnit': "MILLISECONDS"
+  }]
 
-    records = {
-        'Time': str(current_time),
-        'Dimensions': dimensions,
-        'MeasureName': "Active",
-        'MeasureValue': 1,
-        'MeasureValueType': 'DOUBLE'
-    }
-
-    if len(records) > 0:
-      write_records(records)  
-
-def write_records(records):
-    try:
-        result = write_client.write_records(DatabaseName=DATABASE_NAME,
-                                            TableName=TABLE_NAME,
-                                            Records=records,
-                                            CommonAttributes={})
-        status = result['ResponseMetadata']['HTTPStatusCode']
-        print("Processed %d records. WriteRecords Status: %s" %
-              (len(records), status))
-    except Exception as err:
-        print("Error:", err)
-
+  try:
+      result = write_client.write_records(DatabaseName=DATABASE_NAME, TableName=TABLE_NAME,
+                                         Records=record, CommonAttributes=common_attributes)
+      logging.info('Write Record: %(list_0)s - Status: %(list_1)s', { 'list_0': USER, 'list_1' : result['ResponseMetadata']['HTTPStatusCode'] })
+  except write_client.exceptions.RejectedRecordsException as err:
+      logging.error(err)
+      for rr in err.response["RejectedRecords"]:
+          print("Rejected Index " + str(rr["RecordIndex"]) + ": " + rr["Reason"])
+      logging.info("Other records were written successfully. ")
+  except Exception as err:
+      logging.error(err)
+    
 
 def extractText(file):
-    # Call Amazon Textract
-    response = textract.detect_document_text(
-        Document={
-            'S3Object': {
-                'Bucket': s3BucketName,
-                'Name': file.key
-            }
-        })
+  # Call Amazon Textract
+  response = textract.detect_document_text(
+      Document={
+          'S3Object': {
+              'Bucket': s3BucketName,
+              'Name': file.key
+          }
+      })
 
-    print("\nText\n========")
-    text = ""
-    skipText = ["All rooms", "Leave quietly", "Others in the room", "+"]
-    for item in response["Blocks"]:
-        if item["BlockType"] == "LINE":
-            if item["Text"] not in skipText:
-                write(item["Text"])
-                print('' + item["Text"] + '')
-                text = text + " " + item["Text"]
+  text = ""
+  skipText = ["All rooms", "Leave quietly", "Others in the room", "+"]
 
+  # Regex to test extracted value to see if it's the phones clock
+  isClock = re.compile('\d:\d')
+
+  for item in response["Blocks"]:
+      if item["BlockType"] == "LINE":
+          if item["Text"] not in skipText:
+              if isClock.match(item["Text"]):
+                ROOM_TIME = str(round(time.mktime(datetime.datetime.strptime(DATE_OF_EVENT + " " + item["Text"], "%d-%m-%Y %H:%M").timetuple())*1000))
+              else:
+                write(item["Text"], ROOM_TIME)
 
 for file in my_bucket.objects.all():
     extractText(file)
